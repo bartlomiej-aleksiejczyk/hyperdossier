ebc8997c5dd643597352b2846cf643bae7044623
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-12-07 18:44:47 +0100

    feat: add the regular login logout, tweak css

--- common/urls.py
+++ common/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
@@ -6,6 +7,14 @@ urlpatterns = [
         "files/<path:relative_path>",
         views.protected_media,
         name="protected_media",
+    ),
+    path(
+        "accounts/login/",
+        LoginView.as_view(template_name="security/login.html"),
+        name="login",
+    ),
+    path(
+        "accounts/logout/", LogoutView.as_view(next_page="common:login"), name="logout"
     ),
     path("health/", views.health, name="health"),
     path("layout-test/", views.layout_test),
