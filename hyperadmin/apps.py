fa06d2639bbe73934178d4ec58dab6e62cfd0ad0
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-12-11 23:28:07 +0100

    fix: fix admin search provider

--- hyperadmin/apps.py
+++ hyperadmin/apps.py
@@ -3,37 +3,43 @@ from django.urls import reverse
 
 from search.providers import register_global_search_provider
 
+
 def admin_provider_factory(admin_site):
     def admin_search_provider(request, query):
-            results = []
-            for model, model_admin in admin_site._registry.items():
-                search_fields = model_admin.get_search_fields(request)
-                if not search_fields:
-                    continue
+        results = []
 
-                qs = model_admin.get_queryset(request)
-                qs, use_distinct = model_admin.get_search_results(request, qs, query)
-                if use_distinct:
-                    qs = qs.distinct()
+        for model, model_admin in admin_site._registry.items():
+            search_fields = model_admin.get_search_fields(request)
+            if not search_fields:
+                continue
 
-                if not qs.exists():
-                    continue
-                opts = model._meta
-                change_url_name = f"admin:{opts.app_label}_{opts.model_name}_change"
+            qs = model_admin.get_queryset(request)
+            qs, use_distinct = model_admin.get_search_results(request, qs, query)
+            if use_distinct:
+                qs = qs.distinct()
 
-                for obj in qs:  
-                    title = opts.object_name
-                    content = admin_site._get_object_content(obj)
-                    url = reverse(change_url_name, args=[obj.pk])
+            if not qs.exists():
+                continue
+            opts = model._meta
 
-                    results.append(
-                        {
-                            "title": title,
-                            "content": content,
-                            "url": url,
-                        }
-                    )
+            change_url_name = f"admin:{opts.app_label}_{opts.model_name}_change"
+
+            for obj in qs:
+                title = opts.object_name
+                content = admin_site._get_object_content(obj)
+                url = reverse(change_url_name, args=[obj.pk])
+
+                results.append(
+                    {
+                        "title": title,
+                        "content": content,
+                        "url": url,
+                    }
+                )
+        return results
+
     return admin_search_provider
+
 
 class HyperAdminConfig(AppConfig):
     name = "hyperadmin"
